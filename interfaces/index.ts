511e6107b7e798095fbad51d1250dd6bb3458e70
Julio Javier <56454729+juliuuz@users.noreply.github.com>
2022-11-09 21:54:49 -0300

    create interfaces

---
+++ interfaces/index.ts
@@ -0,0 +1,34 @@
+
+export interface ITheme {
+	colors: {
+		primary: string;
+		secondary: string;
+		tertiary: string;
+		success: string;
+		warning: string;
+		danger: string;
+		info: string;
+	};
+	background: {
+		primary: string;
+		secondary: string;
+	};
+	border: {
+		primary: string;
+		secondary: string;
+	};
+	label: {
+		primary: string;
+		secondary: string;
+	};
+	section: {
+		primary: string;
+		secondary: string;
+		tertiary: string;
+	};
+	input: {
+		primary: string;
+		secondary: string;
+		tertiary: string;
+	};
+};
